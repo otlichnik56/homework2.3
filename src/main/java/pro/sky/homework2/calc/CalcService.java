16a2a01e22e1bc4a74fc9c730bc8d9ba8ab2d658
Hyp <otlichnik56@yandex.ru>
2022-08-23 18:13:21 +0500

    Initial commit

---
+++ src/main/java/pro/sky/homework2/calc/CalcService.java
@@ -0,0 +1,32 @@
+package pro.sky.homework2.calc;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalcService {
+
+    public String helloCalc() {
+        return "  Добро пожаловать в калькулятор ";
+    }
+
+    public String plusCalc(int num1, int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    public String minusCalc(int num1, int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    public String multiplyCalc(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    public String divideCalc(double num1, double num2) {
+        if (num2 == 0){
+        return " Ошибка!!!  Деление на 0 не возможно ";
+        } else {
+            return num1 + " / " + num2 + " = " + (num1 / num2);
+        }
+    }
+
+}
