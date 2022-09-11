fab5169d9ed48595cf2cd48d154afca702565cec
Hyp <otlichnik56@yandex.ru>
2022-09-11 23:29:07 +0500

    homework2.12

--- src/main/java/pro/sky/homework2/calc/CalcService.java
+++ src/main/java/pro/sky/homework2/calc/CalcService.java
@@ -23,7 +23,7 @@ public class CalcService {
 
     public String divideCalc(double num1, double num2) {
         if (num2 == 0){
-        return " Ошибка!!!  Деление на 0 не возможно! ";
+        throw new IllegalArgumentException("Ачибка, на нол дэлить низя! Ваще низя!");
         } else {
             return num1 + " / " + num2 + " = " + (num1 / num2);
         }
